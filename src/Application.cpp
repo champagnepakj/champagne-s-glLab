0e91b9f3ce4dd32436022882ced6a4243ba914a3
champagnepaki <zion.dazz@outlook.com>
2025-06-21 18:18:44 +0100

    Implemented rectangle rendering using EBO

--- src/Application.cpp
+++ src/Application.cpp
@@ -18,6 +18,25 @@ const char *fragmentShaderSource = "#version 330 core\n"
 "}\0";
 
 
+/*
+float vertices[] = {	// three vertices (3D position) for triangle
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+*/
+
+float vertices[] = { // rectangle
+	 0.5f,  0.5f, 0.0f, // top right
+	 0.5f, -0.5f, 0.0f, // bottom right
+	-0.5f, -0.5f, 0.0f, // bottom left
+	-0.5f,  0.5f, 0.0f // top left
+};
+
+unsigned int indices[] = { // note that we start from 0!
+	0, 1, 3, // first triangle
+	1, 2, 3 // second triangle
+};
 
 
 
@@ -68,11 +87,7 @@ int main(void)
 		return -1;
 	}
 	
-	float vertices[] = {	// three vertices (3D position) for triangle
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
-	};
+	
 
 	unsigned int VBO;															// initialise an ID for the VBO buffer
 	glGenBuffers(1, &VBO);														// generate the buffer -- parameters(n, buffers)
@@ -134,6 +149,27 @@ int main(void)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+
+
+
+
+
 	//---------------------------------------------------------------------------------
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	
@@ -152,12 +188,13 @@ int main(void)
 								*/
 		
 		/* Render here */
-		glClearColor(0.9f, 0.1f, 0.8f, 1.0f);	// Used to change the colour of the window
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);	// Used to change the colour of the window
 		glClear(GL_COLOR_BUFFER_BIT);			// Clear the colour buffer 
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 		
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
